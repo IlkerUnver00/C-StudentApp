da182c1c2fb0d1e07bf6e749a2ed52b6a43e2a08
İlker Ünver <108666062+IlkerUnver00@users.noreply.github.com>
2022-07-05 13:10:33 +0300

    Add files via upload

---
+++ Vektorel.OgrenciUygulamasi.App/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vektorel.OgrenciUygulamasi.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adınızı giriniz:");
+            string ad = Console.ReadLine();
+            Console.Write("Soyadınız giriniz:");
+            string soyad = Console.ReadLine();
+            Console.Write("No giriniz:");
+            string no = Console.ReadLine();
+
+            //Console.WriteLine(ad+" "+soyad+" "+no);
+            //Console.WriteLine("Ad:{0} Soyad:{1} No:{2}",ad,soyad,no);
+            Console.WriteLine($"Ad:{ad} Soyad:{soyad} No:{no}");
+
+
+            //yazisal,karakter,sayisal,tarih-zaman
+
+            
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
